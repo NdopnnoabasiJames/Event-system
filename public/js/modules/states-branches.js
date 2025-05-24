191dcd00fbeed723c31e50b38ad0106009b19408
Endie-James123 <ndopnnoabasi@gmail.com>
2025-05-24 12:19:10 +0100

    Removed event related functionality from admins dashboard.

---
+++ public/js/modules/states-branches.js
@@ -0,0 +1,13 @@
+// States and branches data structure for Nigeria
+export const statesAndBranches = {
+    'Lagos': ['Lagos Island', 'Lagos Mainland', 'Ikeja', 'Lekki', 'Ajah', 'Ikorodu', 'Epe'],
+    'Abuja': ['Central Area', 'Maitama', 'Wuse', 'Garki', 'Asokoro', 'Gwarinpa'],
+    'Rivers': ['Port Harcourt', 'Obio/Akpor', 'Eleme', 'Oyigbo'],
+    'Kano': ['Kano Municipal', 'Fagge', 'Dala', 'Gwale'],
+    'Oyo': ['Ibadan North', 'Ibadan South', 'Ogbomosho', 'Oyo East'],
+    'Kaduna': ['Kaduna North', 'Kaduna South', 'Zaria', 'Kafanchan'],
+    'Delta': ['Warri', 'Asaba', 'Ughelli', 'Sapele'],
+    'Enugu': ['Enugu North', 'Enugu South', 'Nsukka', 'Udi'],
+    'Anambra': ['Awka', 'Onitsha', 'Nnewi', 'Ekwulobia'],
+    'Imo': ['Owerri', 'Orlu', 'Okigwe', 'Mbaise']
+};
